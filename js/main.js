080edd30c37581713c4b3e7d21a8164e931516c7
Guillermo Montiel <guillermo.montiel@fmc-ag.com>
2023-09-01 09:56:43 -0300

    Cambios para header

--- js/main.js
+++ js/main.js
@@ -20,8 +20,9 @@
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
             $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+            
         } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+            $('.sticky-top').removeClass('shadow-sm').css('top', '0px');
         }
     });
     
