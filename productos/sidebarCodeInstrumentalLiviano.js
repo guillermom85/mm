281d21a22d9822182fb0711d4ff6b99ad21aa185
Guillermo Montiel <guillermo.montiel@fmc-ag.com>
2023-11-07 23:38:54 -0300

    lógica para enviar mensaje

--- productos/sidebarCodeInstrumentalLiviano.js
+++ productos/sidebarCodeInstrumentalLiviano.js
@@ -15,8 +15,11 @@ $(document).ready(function () {
 	});
 	$(".marcasContainer").html("");
 	firstCategory[0].logos.map((logo) => {
+		let brand = logo.split('/').pop().split('.')[0];
+		let message = `Me gustaría obtener información de productos instrumental liviano de tipo: ${currentElement}, específicamente de la marca ${brand}`
+
 		$(".marcasContainer").append(
-			`<a href="#" class="logo col-12 col-md-3"><img class="removeBackground" src="${logo}" alt="Logo" /></a>`
+			`<a href="javascript:enviarContact('${message}');" class="logo col-12 col-md-3"><img class="removeBackground" src="${logo}" alt="Logo" /></a>`
 		);
 	});
 
@@ -37,8 +40,11 @@ $(document).ready(function () {
 		$(".marcasContainer").html("");
 
 		currentCategory[0].logos.map((logo) => {
+			let brand = logo.split('/').pop().split('.')[0];
+			let message = `Me gustaría obtener información de productos instrumental liviano de tipo: ${currentElement}, específicamente de la marca ${brand}`
+
 			$(".marcasContainer").append(
-				`<a href="#" class="logo col-12 col-md-3"><img class="removeBackground" src="${logo}" alt="Logo" /></a>`
+				`<a href="javascript:enviarContact('${message}');" class="logo col-12 col-md-3"><img class="removeBackground" src="${logo}" alt="Logo" /></a>`
 			);
 		});
 	});
