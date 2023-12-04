3f4f9e2beaa7b1e17e16aac9177d57bd4dd205ed
Guillermo Montiel <guillermo.montiel@fmc-ag.com>
2023-12-04 12:23:16 -0300

    fix de descartables

--- productos/sidebarCodeDescartables.js
+++ productos/sidebarCodeDescartables.js
@@ -5,34 +5,17 @@ $(document).ready(function () {
 		$(".wrapper").toggleClass("active");
 	});
 
-	
 	//Carga primer elemento
-	let primerElemento = window.location.hash ? decodeURIComponent(window.location.hash.replace('#', '')) : $(".menu li a").first();
-	primerElemento.addClass("active")
-	$(".contentContainer .title").html(`${primerElemento.text()}`);
-	let firstElement = primerElemento.text().trim();
-	let firstCategory = descartables.filter((element) => {
-		return element.title == firstElement;
-	});
-	$(".marcasContainer").html("");
-	firstCategory[0].logos.map((logo) => {
-		let brand = logo.split('/').pop().split('.')[0];
-		let message = `Me gustaría obtener información de productos descartables de tipo: ${firstElement}, específicamente de la marca ${brand}`
-
-		$(".marcasContainer").append(
-			`<a href="javascript:enviarContact('${message}');" class="logo col-12 col-md-3"><img class="removeBackground" src="${logo}" alt="Logo" /></a>`
-		);
-	})
-	firstCategory[0].items.map((item) => {
-		let message = `Me gustaría obtener información del producto descartables: ${item}`
-
-		$(".prenav").append(
-			`<li onclick="enviarContact('${message}');" class="col-md-6 col-12" style="cursor:pointer;">${item}</li>`
-		);
-	});
+	let primerElemento = window.location.hash ? decodeURIComponent(window.location.hash.replace('#', '')) : $(".menu li a").first().text();
+	setCurrentElement(primerElemento);
 
 	$(".menu li a").click(function (e) {
 		e.preventDefault(); // Prevent default behavior (following the link)
+		let currentElement = $(this).text().trim();
+		setCurrentElement(currentElement);
+	});
+
+	function setCurrentElement(currentElement){
 
 		// Remove active class from all menu items
 		$(".menu li a").removeClass("active");
@@ -42,7 +25,6 @@ $(document).ready(function () {
 
 		$(".contentContainer .title").html(`${$(this).text()}`);
 
-		let currentElement = $(this).text().trim();
 		let currentCategory = descartables.filter((element) => {
 			return element.title == currentElement;
 		});
@@ -65,5 +47,5 @@ $(document).ready(function () {
 				`<li onclick="enviarContact('${message}');" class="col-md-6 col-12" style="cursor:pointer;">${item}</li>`
 			);
 		});
-	});
+	}
 });
